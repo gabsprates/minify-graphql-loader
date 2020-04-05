76413d59c85fd4f0fa760d8f974c20d498230f93
meyer <github.com@meyer.fm>
2020-04-04 19:52:22 -0700

    refactor: replace regex with stripIgnoredCharacters call

--- tests/queryWithVariables.graphql.js
+++ tests/queryWithVariables.graphql.js
@@ -15,7 +15,7 @@ query Movie($filmID: ID!, $id: ID) {
 }
 `;
 
-const expected = `query Movie($filmID:ID!,$id:ID){film(filmID:$filmID,id:$id){title}allFilms{edges{node{title id episodeID}}}}`;
+const expected = `query Movie($filmID:ID!$id:ID){film(filmID:$filmID id:$id){title}allFilms{edges{node{title id episodeID}}}}`;
 
 module.exports = {
   input,
